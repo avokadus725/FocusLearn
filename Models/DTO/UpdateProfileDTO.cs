8af6b39a1b5546febf005f2af4463513c83b9e26
Alona Sudakova <alenasud725@gmail.com>
2024-12-13 00:54:26 +0100

    Data Transfer Object created for working with user profile

---
+++ Models/DTO/UpdateProfileDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FocusLearn.Models.DTO
+{
+    public class UpdateProfileDTO
+    {
+        public string? UserName { get; set; }
+        //public string? Email { get; set; }
+        public string? ProfilePhoto { get; set; }
+        public string? Language { get; set; }
+    }
+}
---
+++ Models/DTO/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace FocusLearn.Models.DTO
+{
+    public class UserDTO
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string? ProfilePhoto { get; set; }
+        public string? Language { get; set; }
+        public string? Role { get; set; }
+        public string? ProfileStatus { get; set; }
+    }
+}
