cc1926a54a4f743fa9371e6af1d30445498448e9
Alona Sudakova <alenasud725@gmail.com>
2024-12-13 00:57:39 +0100

    Interfaces were created to work with services

---
+++ Repositories/Abstract/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using FocusLearn.Models.Domain;
+using System.Security.Claims;
+
+namespace FocusLearn.Repositories.Abstract
+{
+    public interface IAuthService
+    {
+        Task<string> AuthenticateGoogleUserAsync(ClaimsPrincipal principal);
+        Task<string> AuthenticateFacebookUserAsync(ClaimsPrincipal principal);
+    }
+}
---
+++ Repositories/Abstract/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using FocusLearn.Models.Domain;
+using FocusLearn.Models.DTO;
+
+namespace FocusLearn.Repositories.Abstract
+{
+    public interface IUserService
+    {
+        Task<User?> GetUserByProviderAsync(string providerId, string authProvider);
+        Task AddUserAsync(User user);
+        Task<UserDTO?> GetUserByIdAsync(int userId);
+        Task<User?> GetUserByEmailAsync(string email);
+        Task<bool> UpdateUserAsync(int userId, UpdateProfileDTO updateUserDto);
+        Task<bool> DeleteUserAsync(int userId);
+        Task<IEnumerable<UserDTO>> GetAllUsersAsync();
+    }
+}
